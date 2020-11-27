ae3b71e71754ec478b57f795eaeaceccc876982a
Dariusz Syncerek <dsyncerek@outlook.com>
2020-11-27 22:47:45 +0100

    add login button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { CLIENT_ID, TENANT_ID } from '@env';
 import React, { FC } from 'react';
+import { Button } from 'react-native';
+import { authorize } from 'react-native-app-auth';
 import ArticlesList from './screens/ArticlesList/ArticlesList';
 
+const config = {
+  issuer: `https://login.microsoftonline.com/${TENANT_ID}/v2.0`,
+  clientId: CLIENT_ID,
+  redirectUrl: 'com.bgkinformationcenter://oauth/redirect/',
+  scopes: ['openid', 'profile', 'email', 'offline_access'],
+};
+
 const App: FC = () => {
-  return <ArticlesList />;
+  const handleLogin = async () => {
+    try {
+      const authState = await authorize(config);
+      console.log(authState);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <Button title="Login" onPress={handleLogin} />
+      <ArticlesList />
+    </>
+  );
 };
 
 export default App;
---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+declare module '*.png';
+
+declare module '@env' {
+  export const CLIENT_ID: string;
+  export const TENANT_ID: string;
+}
